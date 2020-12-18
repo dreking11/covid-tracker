df232aea0118795d1c3260d68cb09cd1da280703
Dre King <dreking11@gmail.com>
2020-12-18 13:38:31 -0500

    Nothing New

--- components/homecomponent.js
+++ components/homecomponent.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
+import { FINISHED } from '../shared/finished';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+
+function RenderItem({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/IMG_0603.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            finished: FINISHED
+
+       };
+    }
+
 
     static navigationOptions = {
         title: 'Home'
@@ -12,9 +41,9 @@ class Home extends Component {
     render() {
         return (
             <ScrollView>
-                <View>
-                    <Text>Home Component</Text>
-                </View>
+                <RenderItem 
+                    item={this.state.finished.filter(finished => finished.featured)[0]}
+                />
             </ScrollView>
         );
     }
---
+++ shared/finished.js
@@ -0,0 +1,30 @@
+export const FINISHED = [
+    {
+        id: 0,
+        name: "Project Walgreens",
+        image: "/assets/images/IMG_0603.jpg",
+        featured: false,
+        description: "Updated tile floor new register enclosesure."
+    },
+    {
+        id: 1,
+        name: "Project Wal",
+        image: "/assets/images/IMG_0604",
+        featured: false,
+        description: "New counter installation !"
+    },
+    {
+        id: 2,
+        name: "Project Green",
+        image: "/assets/images/IMG_0605",
+        featured: false,
+        description: "New tile floor install."
+    },
+    {
+        id: 3,
+        name: "Hard Work",
+        image: "/assets/images/IMG_0606",
+        featured: true,
+        description: "Entire store update!!"
+    }
+]
